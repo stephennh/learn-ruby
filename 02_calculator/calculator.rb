f877b0fd7edcf079f7c054b1470c8015eef1256f
Stephen H <stephen210huang@hotmail.com>
2017-01-23 17:19:10 -0500

    calculator.rb complete, started on simon_says.rb

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,11 +1,35 @@
 def add(x, y)
-  x + y
+  return x + y
 end
 
 def subtract(x, y)
-  x - y
+  return x - y
 end
 
-def sum
+def sum(array)
+  total = 0
+  array.each do |number|
+    total = total + number
+    end
+  return total
+end
 
+def multiply(x, y)
+  return x * y
+end
+
+def multiply_array(array)
+  total = 1
+  array.each do |number|
+    total = total * number
+    end
+  return total
+end
+
+def factorial(x)
+  total = 1
+  (1..x).each do |x|
+    total = total * x
+    end
+  return total
 end
