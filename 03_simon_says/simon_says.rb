d7d48a5b7d0c9a2f06a565848b75ea744f6980a5
Stephen H <stephen210huang@hotmail.com>
2017-01-23 17:36:50 -0500

    simon_says echo, shout, repeat complete

---
+++ 03_simon_says/simon_says.rb
@@ -0,0 +1,25 @@
+def echo(word)
+  word
+end
+
+def shout(word)
+  shout_word = word.upcase
+  return shout_word
+end
+
+def repeat(word, num_words)
+  total_word = word * num_words
+  return total_word
+end
+
+def start_of_word
+
+end
+
+def first_word
+
+end
+
+def titleize
+
+end
